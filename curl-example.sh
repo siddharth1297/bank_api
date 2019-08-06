1d26d540f58d7f5015186e6961036eed32026b18
Siddharth Nayak <siddharth97nayak@gmail.com>
2019-08-06 20:48:40 +0530

    Updated curl example

--- curl-example.sh
+++ curl-example.sh
@@ -1,11 +1,11 @@
 # Get all the bank details 
+
 echo "------ All the banks details limit=5 and offset=8 ------"
 echo
 
 curl -X GET \
   'https://sidd-bank-api.herokuapp.com/api/bank/?limit=10&offset=8' \
-  -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTY1NDYzNzM0LCJlbWFpbCI6ImFkbWluQGJhbmsuY29tIn0.A57T-L_Rs5OqasQ9l6-MRjoql4ijBgX-gQlTdKJAGks' \
-  -H 'Content-Type: application/json' \
+  -H "Authorization: Bearer $jwt" \
 
 
 # Get bank details with given ifsc
@@ -16,8 +16,7 @@ echo
 
 curl -X GET \
   https://sidd-bank-api.herokuapp.com/api/bank/ABHY0065015/ \
-  -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTY1NDYzNzM0LCJlbWFpbCI6ImFkbWluQGJhbmsuY29tIn0.A57T-L_Rs5OqasQ9l6-MRjoql4ijBgX-gQlTdKJAGks' \
-  -H 'Content-Type: application/json' \
+  -H "Authorization: Bearer $jwt" \
 
 
 # Get bank details with given name and city
@@ -27,8 +26,7 @@ echo "------ All the banks details for name='HDFC BNAK' and city='MUMBAI' with l
 echo
 
 curl -X GET \
-  'https://sidd-bank-api.herokuapp.com/api/bank/HDFC%20BANK/MUMBAI/?limit=5&offset=2' \
-  -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTY1NDYzNzM0LCJlbWFpbCI6ImFkbWluQGJhbmsuY29tIn0.A57T-L_Rs5OqasQ9l6-MRjoql4ijBgX-gQlTdKJAGks' \
-  -H 'Content-Type: application/json' \
+  'https://sidd-bank-api.herokuapp.com/api/search/HDFC%20BANK-MUMBAI/?limit=5&offset=2' \
+  -H "Authorization: Bearer $jwt" \
   
 echo
